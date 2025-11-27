41bcf2e3b5a5e3aa06c4eacb08e11400465e55e1
sagarhursale <rsagar.hursale143@gmail.com>
2025-11-27 23:24:13 +0530

    Update index.js

--- sadhixtest/index.js
+++ sadhixtest/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js!');
+  res.send('Hurry Finally Webpage host with pipeline of ansible+jenkins+docker');
 });
 
 app.listen(port, () => {
